f5364534a79c1e3d0e4333bf528c602a3a4e4cf3
anurudhp <anurudhp97@gmail.com>
2024-06-17 11:33:40 -0700

    call graphs

--- qualtran/bloqs/arithmetic/permutation.py
+++ qualtran/bloqs/arithmetic/permutation.py
@@ -19,18 +19,19 @@ from attrs import field, frozen
 from numpy.typing import NDArray
 
 from qualtran import (
+    Bloq,
+    bloq_example,
     BoundedQUInt,
-    CtrlSpec,
     DecomposeTypeError,
-    GateWithRegisters,
     QAny,
     QBit,
     QDType,
     Signature,
 )
-from qualtran.bloqs.basic_gates import XGate
+from qualtran.bloqs.basic_gates import CNOT, XGate
+from qualtran.bloqs.mcmt.and_bloq import And, MultiAnd
 from qualtran.simulation.classical_sim import ints_to_bits
-from qualtran.symbolics import bit_length, is_symbolic, Shaped, slen, SymbolicInt
+from qualtran.symbolics import bit_length, HasLength, is_symbolic, Shaped, slen, SymbolicInt
 
 if TYPE_CHECKING:
     import sympy
@@ -40,28 +41,8 @@ if TYPE_CHECKING:
     from qualtran.simulation.classical_sim import ClassicalValT
 
 
-def _decompose_permutation_into_cycles(permutation: Sequence[int]) -> Iterator[tuple[int, ...]]:
-    """Generate all non-trivial (more than one element) cycles of a permutation of [0, ..., N - 1]"""
-
-    d = len(permutation)
-    seen = np.full(d, False)
-
-    for i in range(d):
-        if seen[i]:
-            continue
-
-        idx, cycle = i, []
-        while idx < d and not seen[idx]:
-            seen[idx] = True
-            idx = permutation[idx]
-            cycle.append(idx)
-
-        if len(cycle) > 1:
-            yield tuple(cycle)
-
-
 @frozen
-class EqualK(GateWithRegisters):
+class EqualK(Bloq):
     r"""Maps |x>|b> to |x>|b \oplus (x == k)> for a constant k"""
     dtype: QDType
     k: SymbolicInt
@@ -73,17 +54,61 @@ class EqualK(GateWithRegisters):
     def is_symbolic(self):
         return is_symbolic(self.k, self.dtype)
 
-    def build_composite_bloq(self, bb: 'BloqBuilder', **soqs: 'SoquetT') -> Dict[str, 'SoquetT']:
-        x, result = bb.add(
-            XGate().controlled(ctrl_spec=CtrlSpec(qdtypes=self.dtype, cvs=self.k)),
-            ctrl=soqs['input'],
-            q=soqs['result'],
-        )
+    def build_composite_bloq(
+        self, bb: 'BloqBuilder', x: 'Soquet', result: 'Soquet'
+    ) -> Dict[str, 'SoquetT']:
+        if self.is_symbolic():
+            raise DecomposeTypeError(f"cannot decompose symbolic {self}")
+
+        num_qubits = self.dtype.num_qubits
+        (bits_k,) = ints_to_bits(self.k, self.dtype.num_qubits)
+
+        if num_qubits == 1:
+            if self.k == 0:
+                x = bb.add(XGate(), q=x)
+            x, result = bb.add(CNOT(), ctrl=x, target=result)
+            if self.k == 0:
+                x = bb.add(XGate(), q=x)
+        elif num_qubits == 2:
+            control_bloq = And(bits_k[0], bits_k[1])
+
+            xs = bb.split(x)
+            xs, target = bb.add(control_bloq, ctrl=xs)
+            target, result = bb.add(CNOT(), ctrl=target, target=result)
+            xs = bb.add(control_bloq.adjoint(), ctrl=xs, target=target)
+            x = bb.join(xs)
+        else:
+            control_bloq = MultiAnd(tuple(bits_k))
+
+            xs = bb.split(x)
+            xs, junk, target = bb.add(control_bloq, ctrl=xs)
+            target, result = bb.add(CNOT(), ctrl=target, target=result)
+            xs = bb.add(control_bloq.adjoint(), ctrl=xs, junk=junk, target=target)
+            x = bb.join(xs)
+
         return {'x': x, 'result': result}
+
+    def build_call_graph(self, ssa: 'SympySymbolAllocator') -> Set['BloqCountT']:
+        if self.is_symbolic():
+            num_qubits = self.dtype.num_qubits
+            if not is_symbolic(num_qubits) and num_qubits <= 2:
+                if num_qubits == 1:
+                    return {(XGate(), 2), (CNOT(), 1)}
+                else:
+                    cv = ssa.new_symbol('cv')
+                    return {(And(cv, cv), 1), (And(cv, cv).adjoint(), 1), (CNOT(), 1)}
+            else:
+                return {
+                    (MultiAnd(HasLength(num_qubits)), 1),
+                    (MultiAnd(HasLength(num_qubits)).adjoint(), 1),
+                    (CNOT(), 1),
+                }
+
+        return super().build_call_graph(ssa)
 
 
 @frozen
-class XorK(GateWithRegisters):
+class XorK(Bloq):
     r"""Maps |x> to |x \oplus k> for a constant k"""
     bitsize: SymbolicInt
     k: SymbolicInt
@@ -117,12 +142,12 @@ class XorK(GateWithRegisters):
 
 
 @frozen
-class PermutationCycle(GateWithRegisters):
+class PermutationCycle(Bloq):
     """Apply a single permutation cycle on the basis states"""
 
     N: SymbolicInt
-    cycle: Union[tuple[int, ...], Shaped] = field(
-        converter=lambda x: x if isinstance(x, Shaped) else tuple(x)
+    cycle: Union[NDArray[np.integer], Shaped] = field(
+        eq=lambda x: x if isinstance(x, Shaped) else tuple(x.flatten())
     )
 
     @cached_property
@@ -135,13 +160,16 @@ class PermutationCycle(GateWithRegisters):
 
     @cached_property
     def bitsize(self):
-        return bit_length(self.N)
+        return bit_length(self.N - 1)
+
+    def is_symbolic(self):
+        return is_symbolic(self.N, self.cycle)
 
     def build_composite_bloq(self, bb: 'BloqBuilder', q: 'SoquetT') -> dict[str, 'SoquetT']:
         if self.is_symbolic():
             raise DecomposeTypeError(f"cannot decompose symbolic {self}")
 
-        a = bb.allocate(1)
+        a = bb.allocate(dtype=QBit())
 
         for k, x_k in enumerate(self.cycle):
             q, a = bb.add_t(EqualK(self.dtype, x_k), x=q, result=a)
@@ -155,8 +183,16 @@ class PermutationCycle(GateWithRegisters):
 
         return {'q': q}
 
-    def is_symbolic(self):
-        return is_symbolic(self.N, self.cycle)
+    def build_call_graph(self, ssa: 'SympySymbolAllocator') -> Set['BloqCountT']:
+        if self.is_symbolic():
+            x = ssa.new_symbol('x')
+            cycle_len = slen(self.cycle)
+            return {
+                (EqualK(self.dtype, x), cycle_len + 1),
+                (XorK(self.bitsize, x).controlled(), cycle_len),
+            }
+
+        return super().build_call_graph(ssa)
 
     def on_classical_vals(
         self, q: Union['sympy.Symbol', 'ClassicalValT']
@@ -171,12 +207,33 @@ class PermutationCycle(GateWithRegisters):
         return {'q': self.cycle[(index + 1) % len(self.cycle)]}
 
 
+def _decompose_permutation_into_cycles(permutation: Sequence[int]) -> Iterator[NDArray[np.integer]]:
+    """Generate all non-trivial (more than one element) cycles of a permutation of [0, ..., N - 1]"""
+    d = len(permutation)
+    seen = np.full(d, False)
+
+    for i in range(d):
+        if seen[i]:
+            continue
+
+        idx, cycle = i, []
+        while idx < d and not seen[idx]:
+            seen[idx] = True
+            idx = permutation[idx]
+            cycle.append(idx)
+
+        if len(cycle) > 1:
+            yield np.array(cycle)
+
+
 @frozen
-class Permutation(GateWithRegisters):
+class Permutation(Bloq):
     """Apply a permutation of [0, N - 1] on the basis states"""
 
     N: SymbolicInt
-    permutation: Union[NDArray, Shaped]
+    permutation: Union[NDArray[np.integer], Shaped] = field(
+        eq=lambda x: x if isinstance(x, Shaped) else tuple(x.flatten())
+    )
 
     def __attrs_post_init__(self):
         N = self.N
@@ -186,7 +243,10 @@ class Permutation(GateWithRegisters):
 
     @cached_property
     def signature(self) -> Signature:
-        return Signature.build_from_dtypes(q=BoundedQUInt(bit_length(self.N), self.N))
+        return Signature.build_from_dtypes(q=BoundedQUInt(bit_length(self.N - 1), self.N))
+
+    def is_symbolic(self):
+        return is_symbolic(self.N, self.permutation)
 
     def build_composite_bloq(self, bb: 'BloqBuilder', q: 'Soquet') -> dict[str, 'SoquetT']:
         if self.is_symbolic():
@@ -197,8 +257,11 @@ class Permutation(GateWithRegisters):
 
         return {'q': q}
 
-    def is_symbolic(self):
-        return is_symbolic(self.N, self.permutation)
+    def build_call_graph(self, ssa: 'SympySymbolAllocator') -> Set['BloqCountT']:
+        if self.is_symbolic():
+            pass
+
+        return super().build_call_graph(ssa)
 
     def on_classical_vals(
         self, q: Union['sympy.Symbol', 'ClassicalValT']
@@ -206,3 +269,15 @@ class Permutation(GateWithRegisters):
         if is_symbolic(self.permutation):
             raise ValueError("cannot simulate classically on symbolic permuation")
         return {'q': self.permutation[q] if q < len(self.permutation) else q}
+
+
+@bloq_example
+def _permutation_cycle() -> PermutationCycle:
+    permutation_cycle = PermutationCycle(4, np.array([0, 1, 2]))
+    return permutation_cycle
+
+
+@bloq_example
+def _permutation() -> Permutation:
+    permutation = Permutation(4, np.array([1, 3, 0, 2]))
+    return permutation
--- qualtran/bloqs/arithmetic/permutation_test.py
+++ qualtran/bloqs/arithmetic/permutation_test.py
@@ -11,20 +11,71 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-import cirq
 import numpy as np
+import pytest
 
-from qualtran.bloqs.arithmetic.permutation import PermutationCycle
+from qualtran import QBit
+from qualtran.bloqs.arithmetic.permutation import (
+    _permutation,
+    _permutation_cycle,
+    Permutation,
+    PermutationCycle,
+)
+from qualtran.bloqs.basic_gates import CNOT, TGate, XGate
+from qualtran.bloqs.bookkeeping import Allocate, ArbitraryClifford, Free
+from qualtran.resource_counting.generalizers import ignore_split_join
 
 
-def test_permutation_cycle_on_basis_states():
-    cycle = (0, 1, 2)
-    bloq = PermutationCycle(4, cycle)
+def test_examples(bloq_autotester):
+    if bloq_autotester.check_name == 'serialize':
+        pytest.skip()
+
+    bloq_autotester(_permutation_cycle)
+    bloq_autotester(_permutation)
+
+
+def test_permutation_cycle_unitary_and_call_graph():
+    bloq = PermutationCycle(4, np.array([0, 1, 2]))
 
     np.testing.assert_allclose(
-        cirq.unitary(bloq), np.array([[0, 0, 1, 0], [1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0, 1]])
+        bloq.tensor_contract(), np.array([[0, 0, 1, 0], [1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0, 1]])
     )
 
+    _, sigma = bloq.call_graph(generalizer=ignore_split_join)
+    assert sigma == {
+        CNOT(): 8,
+        TGate(): 16,
+        ArbitraryClifford(n=2): 76,
+        Allocate(QBit()): 1,
+        Free(QBit()): 1,
+    }
 
-def test_permutation_bloq_on_random_permutations():
-    pass
+
+def test_permutation_unitary_and_call_graph():
+    bloq = Permutation(7, np.array([1, 2, 0, 4, 3, 5, 6]))
+
+    np.testing.assert_allclose(
+        bloq.tensor_contract(),
+        np.array(
+            [
+                [0, 0, 1, 0, 0, 0, 0, 0],
+                [1, 0, 0, 0, 0, 0, 0, 0],
+                [0, 1, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 1, 0, 0, 0],
+                [0, 0, 0, 1, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 1, 0, 0],
+                [0, 0, 0, 0, 0, 0, 1, 0],
+                [0, 0, 0, 0, 0, 0, 0, 1],
+            ]
+        ),
+    )
+
+    _, sigma = bloq.call_graph(generalizer=ignore_split_join)
+    assert sigma == {
+        CNOT(): 17,
+        TGate(): 56,
+        XGate(): 56,
+        ArbitraryClifford(n=2): 182,
+        Allocate(QBit()): 2,
+        Free(QBit()): 2,
+    }
